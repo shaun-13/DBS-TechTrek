c14afd81b53fd7271bd628a561c350a954f71cdb
Jhocx <jhocx@u.nus.edu>
2020-12-11 12:13:33 +0800

    Balance retrieval API

--- hackathon/src/flask/app.py
+++ hackathon/src/flask/app.py
@@ -18,13 +18,25 @@ def login():
     payload = {"username" : username, "password" : password}
 
     headers = {
-    'x-api-key': X_API_KEY,
+    'x-api-key': API_KEY,
     'Content-Type': 'application/json'
     }
 
     response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
 
-    print(response.text)
+    return response.text
+    
+@app.route("/balance/<custID>")
+def get_balance(custID):
+    API_ENDPOINT = "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/accounts/view"
+    headers = {
+        "x-api-key": "HgdRwVku2V1nD8yNuzaWPxco8RYB9HO8UpnJfIg6"
+    }   
+    body = {
+        'custID': custID
+    }
+
+    response = requests.request("POST", API_ENDPOINT, headers=headers, data=json.dumps(body))
     return response.text
 
 if __name__=='__main__':
