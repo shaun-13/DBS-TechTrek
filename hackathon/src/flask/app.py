c932127448f1aefed988a5d24a8afc9097c26805
Shaun Phua <shaun.phua.2018@sis.smu.edu.sg>
2020-12-11 13:05:49 +0800

    Create login endpoint

--- hackathon/src/flask/app.py
+++ hackathon/src/flask/app.py
@@ -1,61 +1,32 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify, request, json
 import requests
 
 app = Flask(__name__)
 
-# API_KEY = ""
+X_API_KEY = "HgdRwVku2V1nD8yNuzaWPxco8RYB9HO8UpnJfIg6"
 
-# @app.route("/findPlaceFromText/<input_location>")
-# def findPlaceFromText(input_location):
-#     API_ENDPOINT = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key={apiKey}&input={input_location}&inputtype={inputtype}&fields={fields}"
-#     inputtype = "textquery"
-#     fields = "geometry"
-#     url_to_send = API_ENDPOINT.format(apiKey = API_KEY, input_location = input_location, inputtype = inputtype, fields = fields)
-#     payload = {}
-#     headers= {}
-#     response = requests.request("GET", url_to_send, headers=headers, data = payload)
-#     response = make_response(response.json())
-#     response.headers['Access-Control-Allow-Origin'] = '*'
-#     return response
+@app.route("/login", methods=["POST"])
+def login():
+    if not (request.method == "POST"):
+        return 400
 
-# @app.route("/findPlaceFromUserText/<input_location>/<location_bias>")
-# def findPlaceFromUserText(input_location, location_bias):
-#     API_ENDPOINT = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key={apiKey}&input={input_location}&inputtype={inputtype}&fields={fields}&locationbias={locationbias}"
-#     inputtype = "textquery"
-#     fields = "photos,formatted_address,name,rating,opening_hours,geometry,icon"
-#     url_to_send = API_ENDPOINT.format(apiKey = API_KEY, input_location = input_location, inputtype = inputtype, fields = fields, locationbias = location_bias)
-#     payload = {}
-#     headers= {}
-#     response = requests.request("GET", url_to_send, headers=headers, data = payload)
-#     response = make_response(response.json())
-#     response.headers['Access-Control-Allow-Origin'] = '*'
-#     return response
+    login_data = request.json
+    username = login_data['username']
+    password = login_data['password']
 
-# @app.route("/getNearbyPlaces/<lat>/<lng>/<type>")
-# def getNearbyPlaces(lat, lng, type):
-#     API_ENDPOINT = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={apiKey}&location={location}&radius={radius}&type={type}&language=en"
-#     location = lat + "," + lng
-#     radius = '50000' # in metres
-#     url_to_send = API_ENDPOINT.format(apiKey = API_KEY, location = location, radius = radius, type = type)
-#     payload = {}
-#     headers= {}
-#     response = requests.request("GET", url_to_send, headers=headers, data = payload)
-#     response = make_response(response.json())
-#     response.headers['Access-Control-Allow-Origin'] = '*'
-#     return response
+    url = "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/login"
+    payload = {"username" : username, "password" : password}
 
-# @app.route("/getPlacePhoto/<photo_reference>")
-# def getPlacePhoto(photo_reference):
-#     API_ENDPOINT = "https://maps.googleapis.com/maps/api/place/photo?key={apiKey}&photoreference={photo_reference}&maxwidth={max_width}"
-#     max_width = 450
-#     # max_height = 250
-#     url_to_send = API_ENDPOINT.format(apiKey = API_KEY, photo_reference = photo_reference, max_width = max_width)#, max_height = max_height)
-#     payload = {}
-#     headers= {}
-#     response = requests.request("GET", url_to_send, headers=headers, data = payload)
-#     response = make_response(response.url)
-#     response.headers['Access-Control-Allow-Origin'] = '*'
-#     return response
+    headers = {
+    'x-api-key': X_API_KEY,
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+
+    print(response.text)
+    return response.text
+
 
 if __name__=='__main__':
     app.run(port=5002, debug=True)
